defa2e73dc966dd6a4706c863d68e3acd4fe3489
DrCreo <DrCreo1@gmail.com>
2016-09-05 22:57:14 +0100

    Initial update to SharpCord
    Just updated and added some stuff so it works better with SharpCord.

--- DiscordSharp_Starter/DiscordSharp_Starter/Program.cs
+++ DiscordSharp_Starter/DiscordSharp_Starter/Program.cs
@@ -1,30 +1,36 @@
 ﻿//  If you have any questions or just want to talk, join my server!
 //  https://discord.gg/0oZpaYcAjfvkDuE4
-using DiscordSharp;
-using DiscordSharp.Objects;
+using Newtonsoft.Json.Linq;
+using SharpCord;
+using SharpCord.Objects;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace DiscordSharp_Starter
+namespace SharpCord_Starter
 {
     class Program
     {
-        public static DiscordSharp.Objects.DiscordChannel lastchannel;
         public static bool isbot = true;
+        //pf is you're prefix var and is loaded from the Bot_Settings.ini
+        public static string pf = BotSettings.botPrefix;
         static void Main(string[] args)
         {
+            //Checks to see if Bot_Settings.ini exists and if it doesnt it writes to console and ends the program.
+            MakeIni.Create();
             // First of all, a DiscordClient will be created, and the email and password will be defined.
             Console.WriteLine("Defining variables");
             // Fill in token and change isbot to true if you use the API
             // Else, leave token alone and change isbot to false
             // But believe me, the API bots are nicer because of a sexy bot tag!
-            DiscordClient client = new DiscordClient("bot token", isbot);
+            //BotSettings.botToken gets the token from Bot_Settings.ini
+            DiscordClient client = new DiscordClient(BotSettings.botToken, isbot);
             client.ClientPrivateInformation.Email = "email";
             client.ClientPrivateInformation.Password = "pass";
 
@@ -39,20 +45,37 @@ namespace DiscordSharp_Starter
                 // If the bot is connected, this message will show.
                 // Changes to client, like playing game should be called when the client is connected,
                 // just to make sure nothing goes wrong.
-                client.UpdateCurrentGame("DS_starter!", true, "https://github.com/NaamloosDT/DiscordSharp_Starter"); // This will display at "Playing: "
-                //Whoops! i messed up here. (original: Bot online!\nPress any key to close this window.)
+                client.UpdateCurrentGame(BotSettings.botStatus, true, "https://github.com/NaamloosDT/DiscordSharp_Starter"); // This will display at "Playing: "
+                                                                                                                            //Whoops! i messed up here. (original: Bot online!\nPress any key to close this window.)
+
+                //if the bots username is not the same as the name set in the Bot_Settings.ini
+                // this sets the bot name to be the updated name in the Bot_Settings.ini
+                if (client.Me.Username != BotSettings.botName)
+                {
+                    DiscordUserInformation info = new DiscordUserInformation();
+                    info.Username = BotSettings.botName;
+                    client.ChangeClientInformation(info);
+                }
+                //checks to see if a avatar image exists and updates it.
+                if (File.Exists("avatar.jpg"))
+                    client.ChangeClientAvatarFromFile("avatar.jpg");
             };
 
 
             client.PrivateMessageReceived += (sender, e) => // Private message has been received
             {
-                if (e.Message == "!help")
+                if (e.Message == pf + "help")
                 {
                     e.Author.SendMessage("This is a private message!");
                     // Because this is a private message, the bot should send a private message back
                     // A private message does NOT have a channel
                 }
-                if (e.Message.StartsWith("join"))
+                if (e.Message == pf + "update avatar")
+                {
+                    // Because this is a private message, the bot should send a private message back
+                    // A private message does NOT have a channel
+                }
+                if (e.Message.StartsWith(pf +"join"))
                 {
                     if (!isbot) {
                         string inviteID = e.Message.Substring(e.Message.LastIndexOf('/') + 1);
@@ -71,14 +94,15 @@ namespace DiscordSharp_Starter
 
             client.MessageReceived += (sender, e) => // Channel message has been received
             {
-                if (e.MessageText == "!admin")
+                if (e.MessageText == pf + "admin")
                 {
                     bool isadmin = false;
                     List<DiscordRole> roles = e.Author.Roles;
                     foreach(DiscordRole role in roles){
-                        if (role.Name.Contains("Administrator"))
+                        if (role.Name.Contains("Administrator") || e.Author.HasPermission(DiscordSpecialPermissions.Administrator))
                         {
                             isadmin = true;
+                            break;
                         }
                     }
                     if (isadmin)
@@ -90,26 +114,15 @@ namespace DiscordSharp_Starter
                         e.Channel.SendMessage("No, you aren't :c");
                     }
                 }
-                if (e.MessageText == "!help")
+                if (e.MessageText == pf + "help")
                 {
                     e.Channel.SendMessage("This is a public message!");
                     // Because this is a public message, 
                     // the bot should send a message to the channel the message was received.
                 }
-                if (e.MessageText == "!cat")
+                if (e.MessageText == pf + "cat")
                 {
-                    Thread t = new Thread(new ParameterizedThreadStart(randomcat));
-                    t.Start(e.Channel);
-                    string s;
-                    using (WebClient webclient = new WebClient())
-                    {
-                        s = webclient.DownloadString("http://random.cat/meow");
-                        int pFrom = s.IndexOf("\\/i\\/") + "\\/i\\/".Length;
-                        int pTo = s.LastIndexOf("\"}");
-                        string cat = s.Substring(pFrom, pTo - pFrom);
-                        webclient.DownloadFile("http://random.cat/i/" + cat, "cat.png");
-                        client.AttachFile(e.Channel, "meow!", "cat.png");
-                    }
+                    e.Channel.SendMessage("Meow :cat: " + randomcat());
                 }
             };
 
@@ -127,18 +140,16 @@ namespace DiscordSharp_Starter
             //  When a user joins the server, send a message to them.
             client.UserAddedToServer += (sender, e) =>
                 {
-                    e.AddedMember.SendMessage("Welcome to my server! rules:");
-                    e.AddedMember.SendMessage("1. be nice!");
-                    e.AddedMember.SendMessage("- Your name!");
+                    e.AddedMember.SendMessage("Welcome " + e.Guild.Name + ", please read the rules.\nEnjoy you're time here!");
                 };
 
             //  Don't want messages to be removed? this piece of code will
             //  Keep messages for you. Remove if unused :)
-            client.MessageDeleted += (sender, e) =>
+            /*client.MessageDeleted += (sender, e) =>
                 {
                     e.Channel.SendMessage("Removing messages has been disabled on this server!");
                     e.Channel.SendMessage("<@" + e.DeletedMessage.Author.ID + "> sent: " +e.DeletedMessage.Content.ToString());
-                };
+                };*/
 
             
 
@@ -166,9 +177,15 @@ namespace DiscordSharp_Starter
             Environment.Exit(0); // Make sure all threads are closed.
         }
 
-        public static void randomcat(object channel)
+        public static string randomcat()
         {
+            WebClient c = new WebClient();
+            var data = c.DownloadString("http://random.cat/meow");
+            //Console.WriteLine(data);
 
+            JObject o = JObject.Parse(data);
+            string response = o["file"].ToString();
+            return response;
         }
     }
 }
